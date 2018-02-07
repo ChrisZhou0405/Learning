68672bf5ad43bcd677cf86c13945228d1e833626
christ.zhou <christ.zhou@ZU-PC46>
2018-02-07 17:06:19 +0800

    update

---
+++ Log4net_Demo/Common/LogHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using log4net;
+
+namespace Log4net_Demo.Common
+{
+    public class LogHelper
+    {
+        public static ILog log;
+
+        public LogHelper();
+
+        public static void Debug(object message);
+        public static void Debug(string logAppender, object message);
+        public static void Debug(string logAppender, object message, Exception exception);
+        public static void Error(object message);
+        public static void Error(string logAppender, object message);
+        public static void Error(string logAppender, object message, Exception exception);
+        public static void Info(object message);
+        public static void Info(object message, Exception exception);
+        public static void Info(string logAppender, object message, string fileName = null);
+        public static void Info(string logAppender, object message, Exception exception);
+        public static void Warn(object message);
+        public static void Warn(object message, Exception exception);
+        public static void Warn(string logAppender, object message);
+        public static void Warn(string logAppender, object message, Exception exception);
+        public static void WriteLog(Type t, string msg);
+        public static void WriteLog(Type t, Exception ex);
+    }
+}
---
+++ Log4net_Demo/Global.asax.cs
@@ -0,0 +1,51 @@
+﻿using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace Log4net_Demo
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            //应用程序启动时，自动加载配置log4Net  
+            XmlConfigurator.Configure();
+
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
