6f4577081108d54807a664b4479d89b3a44fe322
Chris Zhou <996190648@qq.com>
2018-02-09 14:47:44 +0800

    Test  reference dll

--- Log4net_Demo/Controllers/HomeController.cs
+++ Log4net_Demo/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Log4net_Demo.Common;
-
+using MvcLearn;
 namespace Log4net_Demo.Controllers
 {
     public class HomeController : Controller
@@ -21,6 +21,12 @@ namespace Log4net_Demo.Controllers
                 LogHelper.Error(LogAppender.RollingFileAppender, ex.ToString(), ex);
             }
 
+            return View();
+        }
+
+        public ActionResult ToUpper(string str)
+        {
+            MvcLearn.Common.Utility.ToUpper(str);
             return View();
         }
     }
---
+++ MvcLearn/Common/Utility.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcLearn.Common
+{
+    public class Utility
+    {
+        public static string ToUpper(string test)
+        {
+            return test.ToUpper();
+        }
+    }
+}
