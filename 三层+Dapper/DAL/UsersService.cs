b056acd548243c5454118f790cac9996b9a80283
Christ Zhou <Christ.zhou@bamboonetworks.com>
2019-01-14 18:05:42 +0800

    三层架构+Dapper

---
+++ 三层+Dapper/DAL/UsersService.cs
@@ -0,0 +1,15 @@
+﻿using Models;
+using System.Configuration;
+
+namespace DAL
+{
+   public  class UsersService
+    {
+        private  string connectionStr = ConfigurationManager.AppSettings["Learn"].ToString();
+        public Users GetUser(int uid)
+        {
+
+            return new Users();
+        }
+    }
+}
---
+++ 三层+Dapper/Models/Users.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+   public class Users
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
